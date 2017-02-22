d5d49b17028951423130e87f99f2e6d9f42942ec
Martin Czygan <martin.czygan@gmail.com>
2017-02-22 14:47:15 +0100

    a minimal non-parallel version

--- cmd/microblob/main.go
+++ cmd/microblob/main.go
@@ -1,24 +1,72 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"io"
 	"log"
 	"os"
-
-	"github.com/miku/microblob"
 )
 
-func loggingSink(ch <-chan []microblob.Entry, done chan bool) {
-	for b := range ch {
-		for _, entry := range b {
-			log.Println(entry)
+// Entry associates a key with a section in a file specified by offset and length.
+type Entry struct {
+	Key    string `json:"k"`
+	Offset int64  `json:"o"`
+	Length int64  `json:"l"`
+}
+
+// KeyFunc extracts a key from a blob.
+type KeyFunc func([]byte) (string, error)
+
+// EntryFunc turns a blob to an entry.
+type EntryFunc func([]byte) (Entry, error)
+
+// EntryWriter writes entries to some storage, e.g. a file or a database.
+type EntryWriter func(entries []Entry) error
+
+// LineProcessor read a line, extracts the key and writes entries.
+type LineProcessor struct {
+	r io.Reader   // input data
+	f KeyFunc     // extracts a string key from a byte blob
+	w EntryWriter // serializes entries
+}
+
+// Run start processing the input.
+func (p LineProcessor) Run() error {
+	bw := bufio.NewReader(p.r)
+	for {
+		b, err := bw.ReadBytes('\n')
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		key, err := p.f(b)
+		if err != nil {
+			return err
+		}
+		entry := Entry{Key: key}
+		if err := p.w([]Entry{entry}); err != nil {
+			return err
 		}
 	}
-	done <- true
+	return nil
 }
 
 func main() {
-	opts := microblob.ExtractorOptions{Key: "id", BatchSize: 1000}
-	if err := microblob.Extract(os.Stdin, microblob.NewlineDelimitedJSON, loggingSink, opts); err != nil {
+	processor := LineProcessor{
+		r: os.Stdin,
+		f: func(b []byte) (string, error) {
+			return fmt.Sprintf("x-%d", len(b)), nil
+		},
+		w: func(entries []Entry) error {
+			for _, e := range entries {
+				log.Println(e)
+			}
+			return nil
+		}}
+	if err := processor.Run(); err != nil {
 		log.Fatal(err)
 	}
 }
